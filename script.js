2aae57630c35078bf16864100ba46d7ad1b3a218
Adriana Tarquino <atarquinocespedes@gmail.com>
2023-08-13 13:11:45 -0400

    Primera version 0.1

--- script.js
+++ script.js
@@ -5,3 +5,11 @@ document.getElementById("descargar").addEventListener("click", function() {
     link.download = "cartas";
     link.click();
 });
+
+document.getElementById("Guia").addEventListener("click", function() { 
+    let pdfpath = "https://drive.google.com/file/d/1fQdSz3XlK9iBof0A3T5QNtmfJ3L8Ki_o/view?usp=sharing";
+    let link = document.createElement("guia");
+    link.href = pdfpath;
+    link.download = "guia";
+    link.click();
+});
